669376ba695ac080e2d99f58c872daf729977484
Lincoln <lincolny@ngis.com.au>
2022-05-13 21:33:00 +0800

    home page cut

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'  
+import Image from 'next/image';  
 function Banner() {
   return (
     <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:[600px] 2xl:h-[700px]'>
